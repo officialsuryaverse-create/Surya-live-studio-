8e3c47a9f958a95970b6526f3d02b8372c51520c
Surya Shukla <official.suryaverse@gmail.com>
2026-08-05 01:29:36 +0530

    First project

---
+++ app/src/main/java/com/surya/livestudio/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.surya.livestudio
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val tv = TextView(this)
+        tv.text = "Surya Live Studio"
+        tv.textSize = 24f
+
+        setContentView(tv)
+    }
+}cd ../../res/layout
